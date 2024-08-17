f375e2ec4e47d3558dfb8b41bf409d01b85aba5c
DEEPIKA_BHATT 19 <114798298+Deepikabhatt16@users.noreply.github.com>
2024-08-17 17:04:42 +0530

    Add files via upload

---
+++ src/pages/teacher/TeacherSideBar.js
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { Divider, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+
+import HomeIcon from '@mui/icons-material/Home';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AnnouncementOutlinedIcon from '@mui/icons-material/AnnouncementOutlined';
+import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import SupervisorAccountOutlinedIcon from '@mui/icons-material/SupervisorAccountOutlined';
+
+import { useSelector } from 'react-redux';
+
+const TeacherSideBar = () => {
+    const { currentUser } = useSelector((state) => state.user);
+    const sclassName = currentUser.teachSclass
+
+    const location = useLocation();
+    return (
+        <>
+        <React.Fragment>
+            <ListItemButton component={Link} to="/">
+                <ListItemIcon>
+                    <HomeIcon color={location.pathname === ("/" || "/Admin/dashboard") ? 'primary' : 'inherit'} />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+            </ListItemButton>
+            <ListItemButton component={Link} to="/Admin/classes">
+                <ListItemIcon>
+                    <ClassOutlinedIcon color={location.pathname.startsWith('/Admin/classes') ? 'primary' : 'inherit'} />
+                </ListItemIcon>
+                <ListItemText primary="Classes" />
+            </ListItemButton>
+            
+            <ListItemButton component={Link} to="/Admin/subjects">
+                <ListItemIcon>
+                    <AssignmentIcon color={location.pathname.startsWith("/Admin/subjects") ? 'primary' : 'inherit'} />
+                </ListItemIcon>
+                <ListItemText primary="Subjects" />
+            </ListItemButton>
+            
+            <ListItemButton component={Link} to="/Admin/teachers">
+                <ListItemIcon>
+                    <SupervisorAccountOutlinedIcon color={location.pathname.startsWith("/Admin/teachers") ? 'primary' : 'inherit'} />
+                </ListItemIcon>
+                <ListItemText primary="Teachers" />
+            </ListItemButton>
+            <ListItemButton component={Link} to="/Admin/students">
+                <ListItemIcon>
+                    <PersonOutlineIcon color={location.pathname.startsWith("/Admin/students") ? 'primary' : 'inherit'} />
+                </ListItemIcon>
+                <ListItemText primary="Students" />
+            </ListItemButton>
+         
+            
+        </React.Fragment>
+        <Divider sx={{ my: 1 }} />
+            <React.Fragment>
+                <ListSubheader component="div" inset>
+                    User
+                </ListSubheader>
+                <ListItemButton component={Link} to="/Teacher/profile">
+                    <ListItemIcon>
+                        <AccountCircleOutlinedIcon color={location.pathname.startsWith("/Teacher/profile") ? 'primary' : 'inherit'} />
+                    </ListItemIcon>
+                    <ListItemText primary="Profile" />
+                </ListItemButton>
+                <ListItemButton component={Link} to="/logout">
+                    <ListItemIcon>
+                        <ExitToAppIcon color={location.pathname.startsWith("/logout") ? 'primary' : 'inherit'} />
+                    </ListItemIcon>
+                    <ListItemText primary="Logout" />
+                </ListItemButton>
+            </React.Fragment>
+        </>
+    )
+}
+
+export default TeacherSideBar
