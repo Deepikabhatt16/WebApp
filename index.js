c8be527e8c9075e9c86b244221f0377aab24f54d
DEEPIKA_BHATT 19 <114798298+Deepikabhatt16@users.noreply.github.com>
2024-08-17 16:59:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const cors = require("cors")
+const path=require("path")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// const bodyParser = require("body-parser")
+const app = express()
+const Routes = require("./routes/route.js")
+
+const PORT = process.env.PORT || 5000
+
+dotenv.config();
+
+// app.use(bodyParser.json({ limit: '10mb', extended: true }))
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cors())
+
+app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+  
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+app.use('/', Routes);
+
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
+})
